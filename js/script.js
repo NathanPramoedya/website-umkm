ed42087406fd510fc173c6092ac89a4cd08c4ccd
NathanPramoedya <alvinkuliah554@gmail.com>
2026-09-07 13:20:30 +0700

    feat: buat landing page awal UMKM

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "Promo: Beli 2 Gratis Aku!!!";
+    console.log("Promo Kopi Saji Berhasil Ditampilkan");
+});
