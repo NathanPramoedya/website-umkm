6a2b3541d506e92c5067436afad72bc530a9b43d
NathanPramoedya <alvinkuliah554@gmail.com>
2026-09-12 11:11:13 +0700

    feat: tambah deskripsi produk, dan foto produk

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 Gratis Aku!!!";
-    console.log("Promo Kopi Saji Berhasil Ditampilkan");
+    promoButton.textContent = "Promo: Beli 2 Gratis 1 Cold Brew Aren!";
+    
+    promoButton.style.backgroundColor = "#2f2118";
+    promoButton.style.color = "#ffffff";
+    
+    console.log("Promo Kopi Saji berhasil ditampilkan");
 });
