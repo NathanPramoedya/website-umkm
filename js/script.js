30e8d6aaaf8f1e59150ef85ee4a6a6af2e7134c0
NathanPramoedya <alvinkuliah554@gmail.com>
2026-09-18 06:07:40 +0700

    feat p03: Penambahan navigasi galeri, produk, pembenahan layout dan media

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const promoButton = document.querySelector("#promoButton");
 promoButton.addEventListener("click", () => {
     promoButton.textContent = "Promo: Beli 2 Gratis 1 Cold Brew Aren!";
     
-    promoButton.style.backgroundColor = "#2f2118";
+    promoButton.style.backgroundColor = "#865c42";
     promoButton.style.color = "#ffffff";
     
     console.log("Promo Kopi Saji berhasil ditampilkan");
